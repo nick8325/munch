6a116a84474de532040c6557dd0e08111d8f4573
Nick Smallbone <nick.smallbone@gmail.com>
2011-12-21 13:33:35 +0100

    don't buffer output

--- Test.hs
+++ Test.hs
@@ -4,9 +4,11 @@ import Criterion.Main
 import Data.Int
 import qualified Data.ByteString as BS
 import System.Environment
+import System.IO
 
 test :: (parser -> BS.ByteString -> res) -> parser -> String -> IO ()
 test parse p name = do
+  hSetBuffering stdout LineBuffering
   prog <- getProgName
   file <- fmap (BS.take 2000000) (BS.readFile name)
   defaultMain [bench prog (whnf (parse p) file)]
