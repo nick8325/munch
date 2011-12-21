89f4ecc2274061704a949b6fc55d7d423a93c735
Nick Smallbone <nick.smallbone@gmail.com>
2011-12-21 13:59:36 +0100

    Added prologish parser and removed old tests/ directory.

--- Prolog.hs
+++ Prolog.hs
@@ -1,12 +1,11 @@
-module Test2(parser) where
+module Prolog(parser) where
 
 import Data.Char
-import Test1
-import Parsec
-import Control.Applicative
-import Control.Monad
+import Run
 
-parser :: Parsec Chars Expr
+data Expr = Var Char | Fun Char [Expr]
+
+parser :: Parser Expr
 parser = spaced (fmap Var var <|> liftM2 Fun fun args <|> parens parser)
 
 satisfy_ p = satisfy p >> return ()
@@ -16,14 +15,11 @@ spaced p = aux where aux = (satisfy (== ' ') >> aux) <|> p
 {-# INLINE parens #-}
 parens p = between (satisfy (== '(')) (satisfy (== ')')) p <?> "bracketed expression"
 
--- var = satisfy isUpper' <?> "variable"
--- fun = satisfy isLower' <?> "function"
-var = satisfy (== 'X') <?> "variable"
-fun = satisfy (== 'f') <?> "function"
+var = satisfy isUpper' <?> "variable"
+fun = satisfy isLower' <?> "function"
 
 isUpper' c = c >= 'A' && c <= 'Z'
 isLower' c = c >= 'a' && c <= 'z'
 
 args = spaced (parens (sepBy1 parser (spaced comma)) <|> return [])
 comma = satisfy (== ',') <?> "comma"
--- comma = return ()
