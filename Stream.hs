8679868a9935c8fe63d7d8922d559b2bc8c747b1
Nick Smallbone <nick.smallbone@gmail.com>
2013-10-25 18:21:23 +0200

    First stab at recording first token.

--- Stream.hs
+++ Stream.hs
@@ -6,6 +6,9 @@ import qualified Data.ByteString.Char8 as B
 
 class Stream a where
   type Token a
+  {-# INLINE hd #-}
+  hd :: a -> Maybe (Token a)
+  hd = fmap fst . uncons
   uncons :: a -> Maybe (Token a, a)
 
 instance Stream [a] where
@@ -18,3 +21,19 @@ instance Stream B.ByteString where
   type Token B.ByteString = Char
   {-# INLINE uncons #-}
   uncons = B.uncons
+
+data HeadedBS = Headed {-# UNPACK #-} !Char {-# UNPACK #-} !B.ByteString
+
+{-# INLINE headed #-}
+headed :: B.ByteString -> HeadedBS
+headed x =
+  case B.uncons x of
+    Nothing -> Headed '\000' x
+    Just (x, xs) -> Headed x xs
+
+instance Stream HeadedBS where
+  type Token HeadedBS = Char
+  {-# INLINE hd #-}
+  hd (Headed c _) = Just c
+  {-# INLINE uncons #-}
+  uncons (Headed c bs) = Just (c, headed bs)
