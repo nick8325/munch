bf5403aff32f989fa0284ce1d737317b5599cc5f
Nick Smallbone <nick.smallbone@gmail.com>
2013-10-25 18:43:01 +0200

    Too much sharing in the benchmark!

--- new/Test.hs
+++ new/Test.hs
@@ -13,10 +13,13 @@ import Class
 readBS :: String -> IO B.ByteString
 readBS name = fmap (B.take 2000000) (B.readFile name)
 
+{-# NOINLINE go #-}
+go p file = return (Class.run p file) >>= print
+
 test :: (Stream s, Show a) => IO s -> Look (Simple s) a -> IO ()
 test theFile p = do
   hSetBuffering stdout LineBuffering
   prog <- getProgName
   file <- theFile
-  replicateM_ 100 (print (Class.run p file))
+  replicateM_ 100 (go p file)
   -- defaultMain [bench prog (whnf (parse p) file)]
