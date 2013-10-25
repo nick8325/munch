cde7c10bec144fd4ee20b7613f97074744a56f72
Nick Smallbone <nick.smallbone@gmail.com>
2013-10-25 22:26:26 +0200

    Remove HeadedBS - we perhaps don't need it now

--- Stream.hs
+++ Stream.hs
@@ -18,17 +18,3 @@ instance Stream B.ByteString where
   type Token B.ByteString = Char
   {-# INLINE uncons #-}
   uncons = B.uncons
-
-data HeadedBS = Headed {-# UNPACK #-} !Char {-# UNPACK #-} !B.ByteString
-
-{-# INLINE headed #-}
-headed :: B.ByteString -> HeadedBS
-headed x =
-  case B.uncons x of
-    Nothing -> Headed '\000' x
-    Just (x, xs) -> Headed x xs
-
-instance Stream HeadedBS where
-  type Token HeadedBS = Char
-  {-# INLINE uncons #-}
-  uncons (Headed c bs) = Just (c, headed bs)
