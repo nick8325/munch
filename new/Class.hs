4547711ef6062ded9a4e1760e51281de511dde92
Nick Smallbone <nick.smallbone@gmail.com>
2013-10-25 18:21:23 +0200

    First stab at recording first token.

--- new/Class.hs
+++ new/Class.hs
@@ -12,7 +12,7 @@ class (Functor m, Applicative m, Alternative m, Monad m, MonadPlus m,
 
   {-# INLINE peek #-}
   peek :: m (Maybe (Token (StreamType m)))
-  peek = fmap (fmap fst . uncons) getInput
+  peek = fmap hd getInput
     
   getInput :: m (StreamType m)
   putInput :: StreamType m -> m ()
